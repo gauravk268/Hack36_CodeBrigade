d20f245d4e0c594ae5efd3375f6e34952c093d81
MayankSengar198 <mayanksengar198@gmail.com>
2021-04-10 13:16:23 +0530

    waiting components completed

---
+++ frontend-codebrigade/src/components/Game.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+
+function Game(props){
+    return (
+        <div className="note">
+          <a href={props.link}>
+          <h1>{props.title}</h1>
+          <img src={props.image} width="25" height="15"/>
+          <p>{props.content}</p>
+          </a>
+         
+        </div>
+      );
+}
+export default Game;
