de5e7bbc8c63559bf14012a66a298a3c0f1f2787
Gaurav Kumar <gauravk26800@gmail.com>
2021-04-10 14:19:56 +0530

    added cart component and its functionality added order button in menu

--- frontend-codebrigade/src/components/index.js
+++ frontend-codebrigade/src/components/index.js
@@ -2,8 +2,20 @@ import Navbar from "./Navbar";
 import Home from "./Home";
 import ScanQR from "./ScanQR";
 import Menu from "./Menu";
+import Cart from "./Cart";
+import CartCard from "./CartCard";
 import Waiting from "./Waiting";
 import OrderAgain from "./OrderAgain";
 import Error404 from "./Error404";
 
-export { Navbar, Home, ScanQR, Menu, Waiting, OrderAgain, Error404 };
+export {
+  Navbar,
+  Home,
+  ScanQR,
+  Menu,
+  Cart,
+  CartCard,
+  Waiting,
+  OrderAgain,
+  Error404,
+};
